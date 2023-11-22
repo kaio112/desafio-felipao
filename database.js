dd3e060dd36aa3b86acbb6a0a73e384cf56758f8
kaio112 <kaiofernandoferreira@gmail.com>
2023-11-22 14:19:00 -0300

    segundo commit

--- database.js
+++ database.js
@@ -2,5 +2,4 @@ const {somaPontos, verificaPatente} = require("./funcoes")
 var resultado = somaPontos(40, 5) //recebe vitorias e derrotas 
 let patente = verificaPatente(resultado) //retorna patente
 
-
 console.log("O Herói tem saldo de " + resultado + " pontos está no nível de " + patente)
